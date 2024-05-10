ecc4c69a084205573a25faf057550f31d8deb835
Dong Manh Duc <s3978290@rmit.edu.vn>
2024-05-11 00:28:00 +1000

    added Helper class for general tasks

--- Helper.h
+++ Helper.h
@@ -1,4 +1,26 @@
-#ifndef Helper_H
-#define Helper_H
+#ifndef HELPER_H
+#define HELPER_H
 
-#endif // Helper_H
+#include <cassert>
+#include <iostream>
+#include <random>
+#include <sstream>
+#include <string.h>
+#include <string>
+#include <vector>
+
+class Helper {
+private:
+  Helper();
+
+public:
+  static bool isNumber(std::string s);
+
+  static void splitString(std::string s, std::vector<std::string> &tokens,
+                          std::string delimeter);
+
+  static std::string readInput();
+
+  static void printInvalidInput();
+};
+#endif
