d47804790556535bf230c1a345516da4ad944129
Dong Manh Duc <s3978290@rmit.edu.vn>
2024-05-11 14:43:12 +1000

    added more functions to classes'

--- Register.h
+++ Register.h
@@ -14,6 +14,25 @@ public:
    * Formatted display of coins in balance
    */
   void displayBalance();
+
+  /*
+   * Pay for an item with a denomination
+   * Return the amount left after paying for the cost
+   * If the denomination doesn't fully cover the cost then this method will
+   * return a positive number, if otherwise, it will return a negative number
+   */
+  int pay(int cost, int denomination);
+
+  /*
+   * Check if inputed denomination is a valid denomination
+   */
+  bool isDenomination(int denomination);
+
+  /*
+   * Check if balance has enough of the inputed denomination
+   * It is given that the inputed denomination is valid
+   */
+  bool enoughDenomination(int denomination);
 };
 
 #endif // REGISTER_H
