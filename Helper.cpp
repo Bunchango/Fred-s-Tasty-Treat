4f08e7dc7633cc727db528a2e1955523130dc896
Tran Tuan Trung <s3978165@rmit.edu.vn>
2024-05-14 12:18:12 +1000

    helper.cpp implemented

--- Helper.cpp
+++ Helper.cpp
@@ -2,11 +2,14 @@
 
 using std::string;
 using std::vector;
+#include <iomanip>
+#include <iostream>
+#include <sstream>
 
 Helper::Helper() {}
 
 void Helper::printInvalidInput() {
-  std::cout << "Invalid input.\n" << std::endl;
+    std::cout << "Invalid input.\n" << std::endl;
 }
 
 // This below function does not comply to our C++ Style Guide,
@@ -15,43 +18,55 @@ void Helper::printInvalidInput() {
 // this is not ideal. Nevertheless, feel free to revise this
 // function to make it complied to style guide (this is optional).
 bool Helper::isNumber(string s) {
-  string::const_iterator it = s.begin();
-  char dot = '.';
-  int nb_dots = 0;
-  while (it != s.end()) {
-    if (*it == dot) {
-      nb_dots++;
-      if (nb_dots > 1) {
-        break;
-      }
-    } else if (!isdigit(*it)) {
-      break;
-    }
+    string::const_iterator it = s.begin();
+    char dot = '.';
+    int nb_dots = 0;
+    while (it != s.end()) {
+        if (*it == dot) {
+            nb_dots++;
+            if (nb_dots > 1) {
+                break;
+            }
+        } else if (!isdigit(*it)) {
+            break;
+        }
 
-    ++it;
-  }
-  return !s.empty() && s[0] != dot && it == s.end();
+        ++it;
+    }
+    return !s.empty() && s[0] != dot && it == s.end();
 }
 
 void Helper::splitString(string s, vector<string> &tokens, string delimeter) {
-  tokens.clear();
-  char *_s = new char[s.length() + 1];
-  strcpy(_s, s.c_str());
+    tokens.clear();
+    char *_s = new char[s.length() + 1];
+    strcpy(_s, s.c_str());
 
-  char *pch;
-  pch = strtok(_s, delimeter.c_str());
-  while (pch != NULL) {
-    tokens.push_back(pch);
-    pch = strtok(NULL, delimeter.c_str());
-  }
-  delete[] _s;
+    char *pch;
+    pch = strtok(_s, delimeter.c_str());
+    while (pch != NULL) {
+        tokens.push_back(pch);
+        pch = strtok(NULL, delimeter.c_str());
+    }
+    delete[] _s;
 }
 
 string Helper::readInput() {
-  string input;
-  std::getline(std::cin, input);
+    string input;
+    std::getline(std::cin, input);
 
-  std::cout << std::endl;
+    std::cout << std::endl;
 
-  return input;
+    return input;
+}
+
+std::vector<int> Helper::splitFloat(float number) {
+    int integerPart = static_cast<int>(number);
+    int fractionalDigit = static_cast<int>((number - integerPart) * 10);
+    return {integerPart, fractionalDigit};
+}
+
+std::string Helper::floatToString(float number) {
+    std::ostringstream streamObj;
+    streamObj << std::fixed << std::setprecision(2) << number;
+    return streamObj.str();
 }
