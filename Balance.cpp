23bc450b13409119c2ece63e588f4637121225ae
Nguyen Ba Duc Manh <s3978506@rmit.edu.vn>
2024-05-14 12:10:03 +1000

    implement balance

--- Balance.cpp
+++ Balance.cpp
@@ -1,11 +1,40 @@
 #include "Balance.h"
+#include "Coin.h"
 
 Balance::Balance() {
-  // Create a default balance with no money
+    Denomination denoms[9] = {FIVE_CENTS,   TEN_CENTS,   TWENTY_CENTS,
+                              FIFTY_CENTS,  ONE_DOLLAR,  TWO_DOLLARS,
+                              FIVE_DOLLARS, TEN_DOLLARS, TWENTY_DOLLARS};
+    // Create a default balance with no money
+    for (int i = 0; i < 9; i++) {
+        // Create Coin representing each denomination
+        Coin coin = Coin();
+        coin.count = 0;
+        coin.denom = denoms[i];
+        this->balance[i] = coin;
+    }
 }
 
 int Balance::pay(int cost, int denomination) { return 0; }
 
-bool Balance::enoughDenomination(int denomination) { return true; }
+bool Balance::enoughDenomination(int denomination) {
+    bool result = false;
 
-void Balance::insert(Coin coin) {}
+    for (Coin coin : this->balance) {
+        if (Coin::intToDenomination(denomination) == coin.denom && coin.count > 0) {
+            result = true;
+        }
+    }
+
+    return result;
+}
+
+void Balance::insert(Coin newCoin) {
+    // Check if the inputed Coin match any coin in balance
+    for (Coin &coin : this->balance) {
+        // If the denomination matches then add to the coin in balance
+        if (coin.denom == newCoin.denom) {
+            coin.count += newCoin.count;
+        }
+    }
+}
--- Balance.h
+++ Balance.h
@@ -1,35 +1,44 @@
 #ifndef BALANCE_H
 #define BALANCE_H
 #include "Coin.h"
-#include "Node.h"
 
 #include <vector>
 class Balance {
 public:
-  // We only need a fixed size array of Coin as balance won't change size during
-  // runtime
-  Coin balance[NUM_DENOMS];
+    // We only need a fixed size array of Coin as balance won't change size during
+    // runtime
+    Coin balance[9];
 
-  Balance();
+    Balance();
 
-  /*
-   * Pay for an item with a denomination
-   * Return the amount left after paying for the cost
-   * If the denomination doesn't fully cover the cost then this method will
-   * return a positive number, if otherwise, it will return a negative number
-   */
-  int pay(int cost, int denomination);
+    /*
+     * Pay for an item with a denomination
+     * Return the amount left after paying for the cost
+     * If the denomination doesn't fully cover the cost then this method will
+     * return a positive number, if otherwise, it will return a negative number
+     */
+    int pay(int cost, int denomination);
 
-  /*
-   * Check if balance has enough of the inputed denomination
-   * It is given that the inputed denomination is valid
-   */
-  bool enoughDenomination(int denomination);
+    /*
+     * Check if balance has enough of the inputed denomination
+     * It is given that the inputed denomination is valid
+     */
+    bool enoughDenomination(int denomination);
 
-  /*
-   * Add quantity to a denomination
-   */
-  void insert(Coin coin);
+    /*
+     * Add quantity to a denomination
+     */
+    void insert(Coin newCoin);
+
+    /*
+     * Get the maximum quantity of all denomination
+     */
+    int getMaxQuantity();
+
+    /*
+     * Get the maximum value of all denomination
+     */
+    int getMaxValue();
 };
 
 #endif // BALANCE_H
