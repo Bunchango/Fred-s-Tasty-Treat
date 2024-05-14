b86d3bfa1ef8a8c0af6b20052229af7e5b7bdd42
Nguyen Ba Duc Manh <s3978506@rmit.edu.vn>
2024-05-14 23:28:51 +1000

    implement data manager.h

--- DataManager.h
+++ DataManager.h
@@ -3,48 +3,48 @@
 
 #include "Balance.h"
 #include "LinkedList.h"
+#include <array>
 
 #define EMPTY_SPACE " "
 #define SEPARATOR "|"
 #define SEPARATOR_NUM 2
 #define MONEY_SYMBOL "$"
+#define PRICELEN 7
 #define DENOM_LENGTH 7
-#define QUANTITY_LENGTH 9
-#define VALUE_LENGTH 7
 #define EXCESS_LINE 4
 #define LINE "-"
 
 class DataManager {
 private:
-  LinkedList *meals;
-  Balance balance;
-  // Hold the name of the files so we can save it
-  std::string mealFile;
-  std::string moneyFile;
+    LinkedList *meals;
+    Balance balance;
+    // Hold the name of the files so we can save it
+    std::string mealFile;
+    std::string moneyFile;
 
 public:
-  /*
-   * Read the money data file and meal data file into meals and balance
-   * This method assumes mealFile and moneyFile are valid
-   */
-  DataManager(std::string mealFileName, std::string moneyFileName);
-  ~DataManager();
+    /*
+     * Read the money data file and meal data file into meals and balance
+     * This method assumes mealFile and moneyFile are valid
+     */
+    DataManager(std::string mealFileName, std::string moneyFileName);
+    ~DataManager();
 
-  /*
-   * Save all data to data files
-   * Return true if successfully saved data, false if otherwise
-   */
-  void save();
+    /*
+     * Save all data to data files
+     * Return true if successfully saved data, false if otherwise
+     */
+    void save();
 
-  /*
-   * Formatted display of food items in meals
-   */
-  void displayMeals();
+    /*
+     * Formatted display of food items in meals
+     */
+    void displayMeals();
 
-  /*
-   * Formatted display of coins in balance
-   */
-  void displayBalance();
+    /*
+     * Formatted display of coins in balance
+     */
+    void displayBalance();
 };
 
 #endif // !MANAGER_H
