8fd6fbdbb7dea96a97542ca872145243a8b56a1f
Dong Manh Duc <s3978290@rmit.edu.vn>
2024-05-16 22:06:44 +1000

    reimplement test case feature

--- ftt.cpp
+++ ftt.cpp
@@ -1,8 +1,5 @@
 #include "Helper.h"
-#include "LinkedList.h"
 #include "Machine.h"
-#include <fstream>
-#include <iostream>
 
 /**
  * manages the running of the program, initialises data structures, loads
@@ -12,41 +9,18 @@
 
 int main(int argc, char **argv) {
   // Read arguments into an array of strings
-  std::string strArgs[6];
-  for (int i = 0; i < argc; i++) {
-    std::string strArg(argv[i]);
-    strArgs[i] = strArg;
-  }
+  if (argc <= 5 && argc >= 3) {
+    std::string strArgs[5];
+    for (int i = 0; i < argc; i++) {
+      std::string strArg(argv[i]);
+      strArgs[i] = strArg;
+    }
 
-  // Redirect all outputs to a file if specified
-  std::streambuf *coutbuf = std::cout.rdbuf(); // save old buf
-  std::ofstream outFile;
-  if (strArgs[4] != "") {
-    outFile.open(strArgs[4]);
-    std::cout.rdbuf(outFile.rdbuf()); // redirect cout to file
-  }
-
-  // Read all inputs from a file if specified
-  std::streambuf *cinbuf = std::cin.rdbuf();
-  std::ifstream inFile;
-  if (strArgs[3] != "") {
-    inFile.open(strArgs[3]);
-    std::cin.rdbuf(inFile.rdbuf());
-  }
-
-  Machine *machine = new Machine(strArgs[1], strArgs[2]);
-  machine->start();
-  delete machine;
-
-  // Reset to std output and input
-  std::cout.rdbuf(coutbuf);
-
-  // Close file streams
-  if (outFile.is_open()) {
-    outFile.close();
-  }
-  if (inFile.is_open()) {
-    inFile.close();
+    Machine *machine = new Machine(strArgs[1], strArgs[2]);
+    machine->start();
+    delete machine;
+  } else {
+    Helper::printInvalidInput();
   }
 
   return EXIT_SUCCESS;
