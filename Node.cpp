5cc5a3aeba5df94dad7f514890935bd4510f7942
Tran Tuan Trung <s3978165@rmit.edu.vn>
2024-05-19 00:34:09 +1000

    implement utilitites

--- Node.cpp
+++ Node.cpp
@@ -10,17 +10,17 @@ int Price::valueAsDenom() { return this->value() * 100; }
 
 bool Price::isValidPrice(std::string price) {
   // A valid price is a price that is divisable by 5 after being multipled by
-  // 100
+  // 100 and smaller than 100
   std::vector<std::string> tokens = {};
   Helper::splitString(price, tokens, ".");
   bool result = false;
 
   std::regex pattern(".*[05]$");
-  if (tokens.size() == 2 && Helper::isNumber(price) && tokens[1].size() <= 2 &&
-      std::regex_match(tokens[1], pattern)) {
+  if (tokens.size() == 2 && Helper::isNumber(price) && tokens[1].size() == 2 &&
+      std::regex_match(tokens[1], pattern) && Helper::isValidFloat(price) &&
+      std::stof(price) <= 100) {
     result = true;
   }
-
   return result;
 }
 
@@ -39,6 +39,18 @@ bool FoodItem::isValidIdFormat(const std::string &id) {
 
   // Check if the string matches the pattern
   return std::regex_match(id, std::regex(pattern));
+}
+
+std::string FoodItem::constructID(int id) {
+  // This function assumes id is a valid id, that is id has less than 5
+  // characters
+  std::string result = "F";
+  std::string idAsStr = std::to_string(id);
+  for (int i = 0; i < 4 - idAsStr.size(); i++) {
+    result.append("0");
+  }
+  result.append(idAsStr);
+  return result;
 }
 
 Node::Node() {
